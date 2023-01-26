5f9959db76cc5f83e5ece78f9eb881b9fedf1629
astragenius <darthmalak86@gmail.com>
2023-01-26 21:34:25 +0100

    add - comments

---
+++ src/js/code/Ships.js
@@ -0,0 +1,14 @@
+// class of ships: - carrier - size: 5
+//                 - Battleship - size: 4
+//                 - Cruser - size: 3
+//                 - Submarine - size: 3
+//                 - Destroyer - size: 2
+
+// Das ship object sollte die länge des shiffes beinhalten
+// Das ship object sollte die zahl der treffer beinhalten
+// Das ship object sollte beinhalten ob das shiff gesungen ist oder nicht (boolean value)
+
+/* Ships should have a hit() function that increases the number of ‘hits’ in your ship. */
+
+/* isSunk() should be a function that calculates it based on their length and the number of ‘hits’.
+ */
