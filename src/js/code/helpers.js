157e197e37fa69522f9301e23992c4a11b3b0b2d
astragenius <darthmalak86@gmail.com>
2023-01-27 21:14:55 +0100

    add - add ship lengh and ship size as variables in the helper.js file

---
+++ src/js/code/helpers.js
@@ -0,0 +1,15 @@
+export const SHIP_TYPE = [
+    'Carrier',
+    'Battleship',
+    'Cruiser',
+    'Submarine',
+    'Destroyer',
+]
+
+export const SHIP_LENGTH = {
+    carrier: 5,
+    battleship: 4,
+    cruiser: 3,
+    submarine: 3,
+    destroyer: 2,
+}
