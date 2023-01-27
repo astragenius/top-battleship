628bca9c0c8c1d7b5abdd63820cfed47140092c4
astragenius <darthmalak86@gmail.com>
2023-01-27 22:04:09 +0100

    create - tests for Ship factory function

--- src/js/code/Ships.js
+++ src/js/code/Ships.js
@@ -4,11 +4,16 @@
 //                 - Submarine - size: 3
 //                 - Destroyer - size: 2
 
+// Das ship object beinhaltet den typ des ships
 // Das ship object sollte die länge des shiffes beinhalten
 // Das ship object sollte die zahl der treffer beinhalten
-// Das ship object sollte beinhalten ob das shiff gesungen ist oder nicht (boolean value)
+// Das ship object sollte beinhalten ob das shiff gesunken ist oder nicht (boolean value)
 
 /* Ships should have a hit() function that increases the number of ‘hits’ in your ship. */
 
 /* isSunk() should be a function that calculates it based on their length and the number of ‘hits’.
  */
+
+const Ship = () => {}
+
+export default Ship
---
+++ src/js/test/Ships.test.js
@@ -0,0 +1,50 @@
+import Ship from '../code/Ships'
+
+describe('Testing of the Ship factory function', () => {
+    describe('Testing Ship Properties', () => {
+        const ship = Ship('Carrier')
+
+        test('get the correct ship type', () => {
+            expect(ship.id).toBe('Carrier')
+        })
+        test('length of the ship', () => {
+            expect(ship.length).toBe(5)
+        })
+        test('test default hits', () => {
+            expect(ship.hits).toBe([])
+        })
+    })
+
+    describe('Testing Ship method: getHits() and Hit()', () => {
+        const ship = Ship('Battleship')
+
+        test('Test default return value of getHits', () => {
+            expect(ship.getHit()).toBeEqual([false, false, false, false])
+        })
+        test('Test return value when Ship gets Hit', () => {
+            ship.hit(2)
+            expect(ship.getHit()).toBeEqual([false, false, true, false])
+        })
+        test('Test when hit index is bigger than the ship lenght', () => {
+            expect(ship.hit(8)).toBe('Out of range')
+        })
+    })
+
+    describe('Testing if Ship is sunk', () => {
+        const ship = Ship('Submarine')
+
+        test('Test if ship is not sunk yet', () => {
+            expect(ship.isSunk()).toBeEqual(false)
+        })
+        test('Test if ship is hit but not sunk', () => {
+            ship.hit(3)
+            expect(ship.isSunk()).toBeEqual(false)
+        })
+        test('Test if ship is sunk', () => {
+            ship.hit(0)
+            ship.hit(1)
+            ship.hit(2)
+            expect(ship.isSunk()).toBeEqual(true)
+        })
+    })
+})
