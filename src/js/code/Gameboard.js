8194c834827b6746ab92d8a60638901707bcdd4f
astragenius <darthmalak86@gmail.com>
2023-01-28 17:18:11 +0100

    add - test gameboard array row - col

--- src/js/code/Gameboard.js
+++ src/js/code/Gameboard.js
@@ -1,1 +1,9 @@
-export const GameBoard = () => {}
+export const GameBoard = () => {
+    let gameboard = Array(10)
+        .fill(null)
+        .map(() => Array(10).fill(null))
+
+    const getBoard = () => gameboard
+
+    return { getBoard }
+}
--- src/js/test/gameboard.test.js
+++ src/js/test/gameboard.test.js
@@ -1,1 +1,21 @@
 import { GameBoard } from '../code/Gameboard'
+
+describe('Test GameBoad methods', () => {
+    const board = GameBoard()
+
+    test('Test emty Gameboard', () => {
+        const result = board.getBoard().every((sqr) => sqr === null)
+        const expectResult = false
+        expect(result).toBe(expectResult)
+    })
+    test('Test Gameboard Row', () => {
+        const result = board.getBoard().length
+        const expectResult = 10
+        expect(result).toBe(expectResult)
+    })
+    test('Test Gameboard col', () => {
+        const result = board.getBoard()[0].length
+        const expectResult = 10
+        expect(result).toBe(expectResult)
+    })
+})
