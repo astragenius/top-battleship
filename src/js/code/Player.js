bb95cfb036d4b10068b08f3583f0b82196725e1b
astragenius <darthmalak86@gmail.com>
2023-01-28 16:31:05 +0100

    add - player tests and player function - 3 tests pass

--- src/js/code/Player.js
+++ src/js/code/Player.js
@@ -1,1 +1,11 @@
-export const Player = () => {}
+import { createFleet, SHIP_TYPE } from './helpers'
+export const Player = (type) => {
+    const playerType = type
+    let fleet = createFleet(SHIP_TYPE)
+
+    const getType = () => playerType
+    const getFleet = () => fleet
+    const resetFleet = () => (fleet = createFleet(SHIP_TYPE))
+
+    return { getType, getFleet, resetFleet }
+}
--- src/js/test/player.test.js
+++ src/js/test/player.test.js
@@ -1,3 +1,30 @@
 import { Player } from '../code/Player'
+import { SHIP_TYPE } from '../code/helpers'
+const player1 = Player('Human')
+const player2 = Player('CPU')
 
-describe('Name of the group', () => {})
+describe('Test Player Properties', () => {
+    test('Test player type Human', () => {
+        expect(player1.getType()).toBe('Human')
+    })
+
+    test('Test Player type Computer', () => {
+        expect(player2.getType()).toBe('CPU')
+    })
+})
+
+describe('Test Fleet resett method', () => {
+    test('reset fleet', () => {
+        let fleet1 = player1.getFleet()
+        let fleet2 = player1.getFleet()
+        const equal = fleet1 === fleet2
+        expect(equal).toBe(true)
+    })
+    test('test fleet after reset', () => {
+        let fleet1 = player1.getFleet()
+        player1.resetFleet()
+        let fleet2 = player1.getFleet()
+        const equal = fleet1 === fleet2
+        expect(equal).toBe(false)
+    })
+})
