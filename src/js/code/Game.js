4cd586b6c3824cfcda0f42288fa7007b1bd4f443
astragenius <darthmalak86@gmail.com>
2023-02-08 22:23:06 +0100

    add - create Game handler modul

---
+++ src/js/code/Game.js
@@ -0,0 +1,10 @@
+import { GameBoard } from './Gameboard'
+import { Player } from './Player'
+
+const GameHandler = () => {
+    const player1 = Player('human')
+    const EnemyPlayer = Player('cpu')
+
+    const player1Board = GameBoard()
+    const enemyBoard = GameBoard()
+}
