d1dd00085cf264cf47942a34d514dfc43f51c452
astragenius <darthmalak86@gmail.com>
2023-02-09 21:04:09 +0100

    add - reset game and render grid methods to my game.js module

---
+++ src/js/code/DOM-elements.js
@@ -0,0 +1,6 @@
+export const DOMelements = {
+    playerGrid: document.querySelector('.player__grid'),
+    enemyGrid: document.querySelector('.enemy__grid'),
+    btnNew: document.querySelector('.btn__new-game'),
+    btnReset: document.querySelector('.btn__reset-game'),
+}
--- src/js/code/Game.js
+++ src/js/code/Game.js
@@ -1,5 +1,7 @@
 import { GameBoard } from './Gameboard'
 import { Player } from './Player'
+import renderGameboard from './DOM'
+import { DOMelements } from './DOM-elements'
 
 const GameHandler = () => {
     const player1 = Player('human')
@@ -7,4 +9,24 @@ const GameHandler = () => {
 
     const player1Board = GameBoard()
     const enemyBoard = GameBoard()
+
+    const resetGame = () => {
+        player1.resetFleet()
+        EnemyPlayer.resetFleet()
+        player1Board.resetGameboard()
+        enemyBoard.resetGameboard()
+    }
+
+    const renderGrids = () => {
+        renderGameboard.renderGrid(
+            DOMelements.playerGrid,
+            player1Board,
+            player1.getType()
+        )
+        renderGameboard.renderGrid(
+            DOMelements.enemyGrid,
+            enemyBoard,
+            EnemyPlayer.getType()
+        )
+    }
 }
