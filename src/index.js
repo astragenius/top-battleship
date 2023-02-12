787225485a1f348ebf8ef5bfdf1014ee45af01e7
astragenius <darthmalak86@gmail.com>
2023-02-12 17:08:05 +0100

    fix - add fluid fontsize to modal title

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import './scss/index.scss'
-import { GameBoard } from './js/code/Gameboard'
-import { randomCoords } from './js/code/helpers'
-import Ship from './js/code/ships'
-import { Player } from './js/code/Player'
-import renderGameboard from './js/code/DOM'
 import GameHandler from './js/code/Game'
-import { DOMelements } from './js/code/DOM-elements'
-
 const game = GameHandler()
 
-game.gameInit()
+window.addEventListener('DOMContentLoaded', game.gameInit)
